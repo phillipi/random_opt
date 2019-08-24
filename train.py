c1ed64fa84ff22857a0d455a8a98e6a7d678942e
Phillip Isola <phillip@Phillips-MacBook-Pro.local>
2019-08-24 12:04:58 -0400

    adding mnist training

--- train.py
+++ train.py
@@ -96,6 +96,7 @@ def train(args, seed_start, N, model, device, train_loader, optimizer, epoch):
         '''
         
         if i % args.log_interval == 0:
+            best_acc = np.max(accs)
             print('(iter {}) best acc: {:.0f}%'.format(i, 100*best_acc))
     
     #return best_seed, best_acc

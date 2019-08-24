f7d7ee36b8a820a6e9e2a0ec3fb094304fa16489
Phillip Isola <phillip@Phillips-MacBook-Pro.local>
2019-08-24 01:59:46 -0400

    adding mnist training

--- train.py
+++ train.py
@@ -37,7 +37,7 @@ def weights_init(m):
         #torch.nn.init.kaiming_uniform_(m.weight.data)
         #torch.nn.init.sparse_(m.weight.data, 0.9, std=0.01)
         torch.nn.init.normal_(m.weight.data, mean=0.0, std=1.0)
-        mask = torch.abs(m.weight.data)<2.0
+        #mask = torch.abs(m.weight.data)<2.0
         m.weight.data[mask] = 0
         torch.nn.init.normal_(m.bias, mean=0.0, std=1.0)
     elif isinstance(m, nn.Linear):

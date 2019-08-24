5b28403834a738bf1d341bf86b72cee5aec0fb3f
Phillip Isola <phillip@Phillips-MacBook-Pro.local>
2019-08-24 11:32:34 -0400

    adding mnist training

--- train.py
+++ train.py
@@ -99,14 +99,16 @@ def test(args, model, device, test_loader):
     test_loss = 0
     correct = 0
     with torch.no_grad():
-        for data, target in test_loader:
+        for batch_idx, (data, target) in enumerate(test_loader):
             data, target = data.to(device), target.to(device)
             output = model(data)
             test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
+            if batch_idx>10:
+                break
 
-    test_loss /= len(test_loader.dataset)
+    test_loss /= batch_idx#len(test_loader.dataset)
 
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),

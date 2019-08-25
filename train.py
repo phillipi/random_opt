764d765ab040ba1587250df59ba2e8abc96d7573
Phillip Isola <phillip@Phillips-MacBook-Pro.local>
2019-08-25 02:54:39 -0400

    adding mnist training

--- train.py
+++ train.py
@@ -231,7 +231,7 @@ def main():
             batch_size=args.test_batch_size, shuffle=False, **kwargs)
 
     #N_models_percent = 0.001
-    N_models = 1000
+    N_models = 10
     #models = []
     #for i in range(0,N_models):
     #    models.append(Net().to(device))

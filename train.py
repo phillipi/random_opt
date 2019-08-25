37a0d59983351b504ba4c3dc4d63390526e94704
Phillip Isola <phillip@Phillips-MacBook-Pro.local>
2019-08-25 01:51:05 -0400

    adding mnist training

--- train.py
+++ train.py
@@ -9,11 +9,10 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 import numpy as np
 
-
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 20, 5, 1)
+        self.conv1 = nn.Conv2d(3, 20, 5, 1)
         #self.conv2 = nn.Conv2d(20, 10, 5, 1)
         self.conv2 = nn.Conv2d(20, 50, 5, 1)
         self.fc1 = nn.Linear(4*4*50, 500)

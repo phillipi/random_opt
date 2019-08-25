cfdb26d2da1e243f6a81276d7c85210bb6f101a0
Phillip Isola <phillip@Phillips-MacBook-Pro.local>
2019-08-25 19:20:42 -0400

    adding mnist training

--- train.py
+++ train.py
@@ -67,12 +67,12 @@ def weights_init(m):
         #torch.nn.init.sparse_(m.weight.data, 0.9, std=0.01)
         
         torch.nn.init.normal_(m.weight.data, mean=0.0, std=0.01)
-        mask = torch.abs(m.weight.data)<0.03 # 3.0 seems to be the best...
+        #mask = torch.abs(m.weight.data)<0.03 # 3.0 seems to be the best...
         
         #torch.nn.init.uniform_(m.weight.data,-1.0,1.0)
         #mask = torch.abs(m.weight.data)<0.99
         
-        m.weight.data[mask] = 0
+        #m.weight.data[mask] = 0
         
         torch.nn.init.normal_(m.bias, mean=0.0, std=1.0)
         #torch.nn.init.constant_(m.bias, 0)

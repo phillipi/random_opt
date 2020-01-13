8adedff74f895863874c206378ceb8f94a4ead15
Phillip Isola <phillip@31-35-119.wireless.csail.mit.edu>
2020-01-13 18:10:25 -0500

    testing

--- train.py
+++ train.py
@@ -49,6 +49,7 @@ class NetMNIST(nn.Module):
     '''
 
 class NetCIFAR10(nn.Module):
+    '''
     def __init__(self):
         super(NetCIFAR10, self).__init__()
         self.conv1 = nn.Conv2d(3, 20, 5, 1)
@@ -64,6 +65,15 @@ class NetCIFAR10(nn.Module):
         x = x.view(-1, 5*5*50)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+    '''
+    def __init__(self):
+        super(NetCIFAR10, self).__init__(
+        self.fc1 = nn.Linear(32*32*3, 10)
+
+    def forward(self, x):
+        x = x.view(-1, 32*32*3)
+        x = self.fc1(x)
         return F.log_softmax(x, dim=1)
         
 def weights_init(m):
